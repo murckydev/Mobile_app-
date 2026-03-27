b33c24f7c143584bbc795165cd54c6112cf69532
murckydev <murckydev@gmail.com>
2026-03-27 08:32:06 -0500

    The removal of any kind of emoji inside any print statements, just so the code looks more professional

--- src/main.py
+++ src/main.py
@@ -1,11 +1,22 @@
 from src.logic.calculations import calculate_overtime
 
 def main():
-    # Todo mi flujo de usuario vive aquí, todo esta encapsulado
-    user_input = input("Enter total hours worked: ")
-    total = float(user_input) 
+    while True:
+        user_input = input("Enter total hours worked (or 'q' to quit): ")
+        
+        if user_input.lower() == 'q':
+            return # El usuario decidió salir
+            
+        try:
+            total = float(user_input)
+            break
+
+        except ValueError:
+            print("Error: Please enter a valid number.")
+
+    # Aquí sigue el resto del programa...
     normal, extra = calculate_overtime(total)
-    print(f"Normal Hours: {normal}, Overtime: {extra}")
+    print(f"Normal: {normal}, Extra: {extra}")
 
 if __name__ == "__main__":
     # Solo se ejecuta si corres "python src/main.py"
