0302442ba4a113d6834ec72d2fe946cefacd4086
murckydev <murckydev@gmail.com>
2026-04-08 11:59:12 -0500

    Well, I really messed up. I don't know when I deleted a method necessary for my manager. I also forgot to define the 'is_holiday' function and just gave it an int of 0. I don't know why, don't even ask me. I'm tired, haven't sleep for 23 hours haha... ;--;

--- src/logic/calculations.py
+++ src/logic/calculations.py
@@ -11,35 +11,23 @@ class ShiftCalculator:
         self.night_start_hour = 19
         self.night_end_hour = 6
         self.legal_day_hours = 8
-        self.is_holiday = 0
 
+    def is_holiday(self, dt):
+        """Verifica si es domingo (6) o un festivo (por ahora solo domingos)"""
+        return dt.weekday() == 6
+    
     def _get_night_window(self, start_dt, end_dt):
+        """Define la ventana nocturna de 19:00 a 06:00 del día siguiente"""
         n_start = start_dt.replace(hour=self.night_start_hour, minute=0, second=0, microsecond=0)
-        n_end = end_dt.replace(hour=self.night_end_hour, minute=0, second=0, microsecond=0)
+        # La noche termina a las 6am del día siguiente
+        n_end = n_start + timedelta(hours=11) 
+        
+        # Ajuste por si el turno empezó antes de las 6am
+        if start_dt.hour < self.night_end_hour:
+            n_start -= timedelta(days=1)
+            n_end -= timedelta(days=1)
         return n_start, n_end
 
-    def calculate_shift(self, actual_start, actual_end, scheduled_start, scheduled_end):
-        """Manager: Orquesta todo el proceso"""
-        total = self.calculate_total_duration(actual_start, actual_end)
-
-        # 1. Es festivo o domingo?
-        if self.is_holiday(actual_start):
-
-            holiday_hours = total
-            normal_overtime = 0
-            night = self.get_night_hours(actual_start, actual_end)
-        else:
-            holiday_hours = 0
-            night = self.get_night_hours(actual_start, actual_end)
-            normal_overtime = self.calculate_overtime(actual_start, actual_end, scheduled_start, scheduled_end)
-
-        return {
-            "total": total,
-            "night": night,
-            "overtime": normal_overtime,
-            "holiday": holiday_hours,
-            "status": RecordStatus.PENDING
-        }
 
     def get_night_hours(self, start_dt, end_dt):
         n_start, n_end = self._get_night_window(start_dt, end_dt)
@@ -55,7 +43,7 @@ class ShiftCalculator:
         if end_dt <= start_dt: return 0
         return (end_dt - start_dt).total_seconds() / 3600
 
-    def calculate_overtime(self, scheduled_start, scheduled_end, actual_start, actual_end):
+    def calculate_overtime(self, actual_start, actual_end, scheduled_start, scheduled_end):
         if scheduled_end < scheduled_start:
             scheduled_end += timedelta(days=1)
         if actual_end < actual_start:
@@ -97,3 +85,27 @@ class ShiftCalculator:
             report["is_compliant"] = False
 
         return report
+    
+    # --- ALL BACKGROUND METHODS ---^
+
+    def calculate_shift(self, actual_start, actual_end, scheduled_start, scheduled_end):
+        total_val = self.calculate_total_duration(actual_start, actual_end)
+
+        if self.is_holiday(actual_start):
+            h_hours = total_val
+            o_hours = 0
+            n_hours = self.get_night_hours(actual_start, actual_end)
+        else:
+            h_hours = 0
+            n_hours = self.get_night_hours(actual_start, actual_end)
+            o_hours = self.calculate_overtime(actual_start, actual_end, scheduled_start, scheduled_end)
+
+        return {
+            "total": total_val,
+            "night": n_hours,
+            "overtime": o_hours,
+            "holiday": h_hours,
+            "status": RecordStatus.PENDING
+        }
+    
+    # --- MANAGER ---^ 
